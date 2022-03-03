d8295fc274830c85347e3c6924c83ac7d769d2e3
JEONGJIYOUN <jjy134347@gmail.com>
2022-03-03 20:32:36 +0900

    add#3 hamburger button add

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
 import com.example.todoapp.ui.main.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.main_drawer)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
--- app/src/main/java/com/example/todoapp/ui/main/fragment/MainFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/main/fragment/MainFragment.kt
@@ -1,11 +1,12 @@
 package com.example.todoapp.ui.main.fragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.todoapp.R
 import com.example.todoapp.ui.main.viewmodel.MainViewModel
 
@@ -25,14 +26,9 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    fun openDrawers() {
 
-    fun onStartBtnClicked(){
-
+        view?.findViewById<DrawerLayout>(R.id.navigation_view)?.openDrawer(0);
     }
 
 }
