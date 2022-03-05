c021ffc76b516098ccf56ef11edd47d9877fae2f
JEONGJIYOUN <jjy134347@gmail.com>
2022-03-05 17:20:30 +0900

    add#23_developing Room Database

--- app/src/main/java/com/example/todoapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/main/MainViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.todoapp.ui.main
 
 import androidx.lifecycle.ViewModel
+import com.example.todoapp.dao.UserDao
+import com.example.todoapp.data.User
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(val userDao: UserDao) : ViewModel() {
 
+    init {
+        //testRoom()
+    }
 
     fun onClickStartbtn(){
         //TODO("Not yet implemented")
@@ -14,4 +18,13 @@ class MainViewModel : ViewModel() {
         //TODO("Not yet implemented")
     }
 
+    private fun testRoom(){
+        var user = User()
+        user.uid = "test1"
+        user.profile = "1"
+        user.email = "1"
+        user.username = "1"
+
+        userDao.insertUser(user)
+    }
 }
